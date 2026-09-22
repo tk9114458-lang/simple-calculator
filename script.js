9f4fdf835c1cfed4a95af2d7b231788b70050c9f
tk9114458-lang <tk9114458@gmail.com>
2026-09-22 19:13:02 +0500

    Improve calculator HTML, CSS, and JavaScript

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-function add() {
+function getNumbers() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
 
-    document.getElementById("result").innerText = num1 + num2;
+    return [num1, num2];
+}
+
+function add() {
+    let numbers = getNumbers();
+    document.getElementById("result").innerText = numbers[0] + numbers[1];
 }
 
 function subtract() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-
-    document.getElementById("result").innerText = num1 - num2;
+    let numbers = getNumbers();
+    document.getElementById("result").innerText = numbers[0] - numbers[1];
 }
 
 function multiply() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-
-    document.getElementById("result").innerText = num1 * num2;
+    let numbers = getNumbers();
+    document.getElementById("result").innerText = numbers[0] * numbers[1];
 }
 
 function divide() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
+    let numbers = getNumbers();
 
-    if (num2 == 0) {
+    if (numbers[1] == 0) {
         document.getElementById("result").innerText = "Cannot divide by 0";
     } else {
-        document.getElementById("result").innerText = num1 / num2;
+        document.getElementById("result").innerText = numbers[0] / numbers[1];
     }
+}
+
+function clearCalculator() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").innerText = "0";
 }
